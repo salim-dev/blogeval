ba6d8445dfce986d383ab7ea8543af5ad4093006
salim-dev <salim.elmoussaoui.afpa2017@gmail.com>
2017-03-30 14:41:35 +0200

    create connectDB and dbConfig

---
+++ src/config/dbConfig.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package config;
+
+/**
+ *
+ * @author Salim El Moussaoui <salim.elmoussaoui.afpa2017@gmail.com>
+ */
+public class dbConfig {
+    public static final String DB_NAME = "";
+    public static final String DB_USER = "";
+    public static final String DB_PASSWORD = "";
+}
